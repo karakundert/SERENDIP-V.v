4b431c3f70493446bd7c5c55bc007aa44b7669b2
Kara Kundert <kara@feynman.(none)>
2012-01-12 11:25:17 -0800

    Modified specplot.py to plot the hits.

--- code/specplot.py
+++ code/specplot.py
@@ -16,13 +16,19 @@ for inputfilename in sys.argv[1:]:
         filesreader = CSV.DictReader(file, skipinitialspace=True)
         x = []
         y = []
+        z = []
         for each in filesreader:
             x.append(int(each['coarsebin']))
-            y.append(float(each['power']))
+            y.append(float(each['coarsepower']))
+            z.append(float(each['hitpower']))
+
+        print x
             
         X = numpy.array(x)
         Y = numpy.array(y)
+        Z = numpy.array(z)
         plt.semilogy(X, Y)
+        plt.scatter(X, Z)
 
 plt.xlabel('coarse bin')
 plt.ylabel('power')
