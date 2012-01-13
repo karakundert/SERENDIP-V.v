f921a1dd6e0a88b4ed3063ba942ff40de37836e8
Kara Kundert <kkundert@sting.eecs.berkeley.edu>
2012-01-13 12:17:55 -0800

    Made a structure for the header data to be read into.

---
+++ code/setidata.C
@@ -0,0 +1,51 @@
+struct setidata {
+    long header_size;
+    long data_size;
+    char[255] name;
+    long dsi;
+    long frameseq;
+    long dataseq;
+    long idlecount;
+    long missed;
+    long ast;
+    char[255] receiver;
+    double samplerate;
+    double ver;
+    // SCRAM data goes here
+    long agc_systime;
+    double agc_az;
+    long agc_time;
+    double agc_lst;
+    double agc_ra;
+    double agc_dec;
+    long alfashm_systime;
+    long alfashm_alfafirstbias;
+    long alfashm_alfasecondbias;
+    double alfashm_alfamotorposition;
+    long if1_systime;
+    double if1_syni_freqhz_0;
+    long if1_syni_ampdb_0;
+    double if1_rffreq;
+    double if1_if1frqmhz;
+    long if1_alfafb;
+    long if2_systime;
+    long if2_usealfa;
+    long tt_systime;
+    long tt_turretencoder;
+    double tt_turretdegrees;
+    long min_synth_freq;
+    long max_synth_freq;
+    long min_rec_freq;
+    long max_rec_freq;
+    long filtered_min_rec_freq;
+    long filtered_max_rec_freq;
+    // repeat of sample rate and receiver information excluded in this
+    // structure
+    long num_m_in_d;
+    long num_diskbufs;
+    char[255] synth_model;
+    double turret_degrees_alfa;
+    long turret_degrees_tolerance;
+}
+
+
